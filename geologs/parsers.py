17cad99776a8de82557df622926a245b430be42a
gyndlf <james@zingel.co.nz>
2024-11-29 18:18:13 +1100

    Updated SSH parsers

--- geologs/parsers.py
+++ geologs/parsers.py
@@ -44,14 +44,24 @@ def monty(message: str) -> str:
 def ssh(message: str) -> str:
     """Parse message from sshd."""
     comps = message.split(" ")
+    date = " ".join(comps[0:3])
+    log = " ".join(comps[5:])
+
+    icon = " "
     if "accepted publickey" in message.lower():
         # Confirmation of public key
-        return ":key: " + " ".join(comps[5:13])
+        icon = " :key: "
     elif "session opened" in message.lower():
         # Creation of a new session
-        return ":satellite_antenna: " + " ".join(comps[5:])
-    else:
-        return message
+        icon = " :satellite_antenna: "
+    elif "error: kex_exchange_identification" in message.lower():
+        # Probably malicious port scanning
+        icon = " :warning: "
+    elif "invalid format" in message.lower():
+        # Most likely someone using HTTP scanning
+        icon = " :military_helmet: "
+
+    return date + icon + log
 
 
 
@@ -66,3 +76,5 @@ if __name__ == "__main__":
     print(monty("[2024-09-10 11:28:44,276] INFO Run finished and took 3 seconds"))
     print(ssh("Sep 13 06:17:24 hostname sshd[281443]: Accepted publickey for user from 1.1.1.1 port 22 ssh2: RSA SHA256:HASH"))
     print(ssh("Sep 13 06:17:24 hostname sshd[281443]: pam_unix(sshd:session): session opened for user user(uid=1001) by (uid=0)"))
+    print(ssh("Nov 22 11:08:36 hostname sshd[2700138]: error: kex_exchange_identification: banner line contains invalid characters"))
+    print(ssh("Nov 25 06:20:08 hostname sshd[2834581]: banner exchange: Connection from 1.1.1.1 port 10301: invalid format"))
